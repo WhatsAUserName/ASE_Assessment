f7b8717f78c36fc44f3f98acedc9f9238d59180a
Sam <samvinzel@gmail.com>
2020-01-06 14:48:29 +0000

    Added test menu

---
+++ ASE_Assessment/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool exitMenu = false;
+
+
+
+int main() 
+{
+	cout << "Test Menu"<<endl;
+	while (true) 
+	{
+		//cout << endl;
+		if (exitMenu == true) 
+		{
+			break;
+		}
+
+	}
+}

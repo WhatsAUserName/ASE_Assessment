12782de018da74015dd0e9711c31ade42f3df2d4
Sam <samvinzel@gmail.com>
2020-01-06 14:56:24 +0000

    menu works

--- ASE_Assessment/Source.cpp
+++ ASE_Assessment/Source.cpp
@@ -5,6 +5,26 @@ using namespace std;
 
 bool exitMenu = false;
 
+string input;
+
+void mainMenu() 
+{
+	cout << "Q-Quit" << endl;
+	cout << "I-Insert" << endl;
+	cout << "D-Delete" << endl;
+	cout << "C-Contents" << endl;
+	cout << "S-Save to file" << endl;
+	cout << "L-Load from file" << endl;
+	cout << "R-Linear Search" << endl;
+	cin >> input;
+
+	if (input == "Q" || input == "q") 
+	{
+		exitMenu = true;
+	}
+
+
+}
 
 
 int main() 
@@ -17,6 +37,6 @@ int main()
 		{
 			break;
 		}
-
+		mainMenu();
 	}
 }
